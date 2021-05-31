e6ab2986090dc7a72a4fa35dd83e050744cf5ad0
Alex Nichol <unixpickle@gmail.com>
2021-05-31 12:04:01 -0400

    penguin model renders

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"github.com/unixpickle/model3d/model3d"
+	"github.com/unixpickle/model3d/render3d"
+)
 
+func main() {
+	penguin := NewPenguin()
+
+	mesh := model3d.MarchingCubesSearch(penguin, 0.01, 8)
+	SaveMesh(mesh, penguin)
+	RenderMesh(mesh, penguin)
+}
+
+func SaveMesh(mesh *model3d.Mesh, o Object) {
+	vertexColor := func(c model3d.Coord3D) [3]float64 {
+		r, g, b := render3d.RGB(o.Color(c))
+		return [3]float64{r, g, b}
+	}
+	mesh.SaveMaterialOBJ("export.zip", model3d.VertexColorsToTriangle(vertexColor))
+}
+
+func RenderMesh(mesh *model3d.Mesh, o Object) {
+	colorFunc := func(c model3d.Coord3D, rc model3d.RayCollision) render3d.Color {
+		return o.Color(c)
+	}
+	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 400, colorFunc)
 }
---
+++ penguin.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/model3d/model2d"
+	"github.com/unixpickle/model3d/model3d"
+	"github.com/unixpickle/model3d/render3d"
+)
+
+type Penguin struct {
+	model3d.Solid
+}
+
+func NewPenguin() *Penguin {
+	r, err := os.Open("models/penguin/files/Penguin_t.stl")
+	essentials.Must(err)
+	triangles, err := model3d.ReadSTL(r)
+	essentials.Must(err)
+	mesh := model3d.NewMeshTriangles(triangles).Scale(1.0 / 40.0)
+	// Center the mesh on x/y, start it at z=0.
+	mesh = mesh.Translate(mesh.Min().Mid(mesh.Max()).Scale(-1))
+	mesh = mesh.Translate(model3d.Z(-mesh.Min().Z))
+	fmt.Println(mesh.Min(), mesh.Max())
+	return &Penguin{
+		Solid: model3d.NewColliderSolid(model3d.MeshToCollider(mesh)),
+	}
+}
+
+func (p *Penguin) Color(c model3d.Coord3D) render3d.Color {
+	if c.Y < 0 {
+		// Front patch of white.
+		proj := c.XZ()
+		center := model2d.XY(0, 0.8)
+		radii := model2d.XY(0.2, 0.4)
+		if center.Div(radii).Dist(proj.Div(radii)) < 1.0 {
+			return render3d.NewColor(1.0)
+		}
+	}
+	return render3d.NewColor(0.1)
+}
