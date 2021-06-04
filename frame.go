a3634c23ecf2d8830555e891b970992e831327f9
Alex Nichol <unixpickle@gmail.com>
2021-06-04 14:16:14 -0400

    start styling the frame

--- frame.go
+++ frame.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"math"
+
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
+)
+
+const (
+	FrameThickness    = 0.2
+	FrameColorEpsilon = 0.02
 )
 
 type Frame struct {
@@ -12,12 +19,25 @@ type Frame struct {
 func NewFrame() Frame {
 	return Frame{
 		Solid: &model3d.SubtractedSolid{
-			Positive: model3d.NewRect(model3d.XYZ(-2.5, -0.8, -0.2), model3d.XYZ(2.5, 0.8, 5.0)),
-			Negative: model3d.NewRect(model3d.XYZ(-2.3, -0.81, 0.0), model3d.XYZ(2.3, 0.81, 4.8)),
+			Positive: model3d.NewRect(
+				model3d.XYZ(-2.5, -0.8, -FrameThickness),
+				model3d.XYZ(2.5, 0.8, 5.0),
+			),
+			Negative: model3d.NewRect(
+				model3d.XYZ(-2.5+FrameThickness, -0.8+FrameThickness, 0.0),
+				model3d.XYZ(2.5-FrameThickness, 0.81, 5.0-FrameThickness),
+			),
 		},
 	}
 }
 
-func (f Frame) Color(model3d.Coord3D) render3d.Color {
-	return render3d.NewColorRGB(0.5, 0.5, 1.0)
+func (f Frame) Color(c model3d.Coord3D) render3d.Color {
+	min, max := f.Min(), f.Max()
+	if math.Abs(model3d.NewRect(min, max).SDF(c)) < FrameColorEpsilon {
+		return render3d.NewColor(0.5)
+	}
+	if c.Z <= min.Z+FrameThickness+FrameColorEpsilon {
+		return render3d.NewColorRGB(0.1, 0.8, 0.1)
+	}
+	return render3d.NewColorRGB(0.0, 190.0/255, 1.0)
 }
