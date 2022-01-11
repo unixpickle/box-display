71d7a97178ca2678f281cef7b459ce0ad4181a13
Alex Nichol <unixpickle@gmail.com>
2022-01-11 13:14:01 -0800

    leverage toolbox3d color func API

--- object.go
+++ object.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 const (
@@ -50,15 +51,14 @@ func (t *transformedObject) Color(c model3d.Coord3D) render3d.Color {
 
 type joinedObject struct {
 	model3d.Solid
-	objects []Object
-	sdfs    []model3d.SDF
 	bounds  []*model3d.Rect
+	colorFn toolbox3d.CoordColorFunc
 }
 
 func Join(objs ...Object) Object {
 	js := make(model3d.JoinedSolid, len(objs))
-	sdfs := make([]model3d.SDF, len(objs))
 	bounds := make([]*model3d.Rect, len(objs))
+	var colorArgs []interface{}
 	for i, obj := range objs {
 		js[i] = obj
 		size := obj.Max().Sub(obj.Min())
@@ -68,30 +68,17 @@ func Join(objs ...Object) Object {
 		}
 		eps := math.Max(size.X, math.Max(size.Y, size.Z)) * prec
 		mesh := model3d.MarchingCubesSearch(obj, eps, 8)
-		sdfs[i] = model3d.MeshToSDF(mesh)
+		sdf := model3d.MeshToSDF(mesh)
+		colorArgs = append(colorArgs, sdf, obj.Color)
 		bounds[i] = model3d.NewRect(mesh.Min(), mesh.Max())
 	}
 	return &joinedObject{
 		Solid:   js,
-		objects: objs,
-		sdfs:    sdfs,
 		bounds:  bounds,
+		colorFn: toolbox3d.JoinedCoordColorFunc(colorArgs...),
 	}
 }
 
 func (j *joinedObject) Color(c model3d.Coord3D) render3d.Color {
-	maxSDF := math.Inf(-1)
-	var closest Object
-	for i, obj := range j.objects {
-		// Eliminate far away bounds before checking SDF.
-		if j.bounds[i].SDF(c) < maxSDF {
-			continue
-		}
-		sdf := j.sdfs[i].SDF(c)
-		if sdf > maxSDF {
-			maxSDF = sdf
-			closest = obj
-		}
-	}
-	return closest.Color(c)
+	return j.colorFn(c)
 }
